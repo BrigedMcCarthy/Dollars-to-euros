68e004a9b5b845b5105540658f28908cf5f724d5
brigedwghs <168587753+BrigedMcCarthy@users.noreply.github.com>
2024-10-23 10:40:33 -0500

    changed int to float and removed if statement

--- main.py
+++ main.py
@@ -8,9 +8,12 @@ print('Briged McCarthy 10/23/24')
 usrchoice = input("Would you like to convert dollars to euros?")
 
 while usrchoice == "yes":
-    dollar = int(input("Amount of $: "))
+    dollar = float(input("Amount of $: "))
     euro = dollar * .94540
     print(euro)
-    usr = input("would you like to convert again? yes/no: ") #I love when a single parenthesis breaks the code
-    if usr == "no":
-        break
+    usrchoice = input("Would you like to convert dollars to euros again?")
+
+while usrchoice == "yes":
+    dollar = float(input("Amount of $: "))
+    euro = dollar * .94540
+    print(euro)
